db867b32057b4b4cf792a5d57e6bb6111e09fe02
Jamisha Bell <Jamishabell2@gmail.com>
2026-01-08 13:22:50 -0600

    first commit

---
+++ functions_and_packages.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+spring_classes = ["Analysis and Design of Information Systems", 4125, "Cloud Computing for Business II", 4130, "Introduction Data Mining", 4141, "Strategic Management", 3830]
+
+# Create numpy array from spring_classes
+np_spring_classes = np.array(spring_classes)
+
+print(np_spring_classes)
+print(type(np_spring_classes))
